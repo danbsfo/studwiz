f77ca9f4679eb1dea477c5ab988ec5e3cb5ebf2b
Dan <mrdaniel@postinbox.com>
2014-08-15 21:18:02 -0400

    adding mongo into the mix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Studwiz' });
 });
+
+MongoClient.connect("mongodb://localhost:27017/studwiz", function(err, db) {
+  if(!err)
+    console.log("connected");
+  else
+    console.log("problem connecting");
+})
 
 module.exports = router;
